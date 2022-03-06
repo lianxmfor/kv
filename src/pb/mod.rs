d98f00c4a6a2f07030f55c759117417ba5862fc4
lianxmfor <1034552569@qq.com>
2022-03-06 21:12:12 +0800

    feat(pb/Kvpair): add method new

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,1 +1,12 @@
 pub mod abi;
+
+use abi::*;
+
+impl Kvpair {
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
