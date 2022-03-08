7b5ae7e00445887ac24262a7fed39193e0008763
lianxmfor <1034552569@qq.com>
2022-03-08 10:46:45 +0800

    feat(pb): impl from Value,KvError to CommandResponse trait

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -31,3 +31,31 @@ impl From<&str> for Value {
         }
     }
 }
+
+impl From<Value> for CommandResponse {
+    fn from(v: Value) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            values: vec![v],
+            ..Default::default()
+        }
+    }
+}
+
+impl From<KvError> for CommandResponse {
+    fn from(e: KvError) -> Self {
+        let mut r = CommandResponse {
+            status: StatusCode::INTERNAL_SERVER_ERROR.as_u16() as _,
+            message: e.to_string(),
+            ..Default::default()
+        };
+
+        match e {
+            KvError::NotFound(_, _) => r.status = StatusCode::NOT_FOUND.as_u16() as _,
+            KvError::InvalidCommand(_) => r.status = StatusCode::BAD_REQUEST.as_u16() as _,
+            _ => {}
+        }
+
+        r
+    }
+}
