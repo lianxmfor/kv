78e7b2e77ae7bc6e59289b945b9c05ca7c2673ef
lianxmfor <1034552569@qq.com>
2022-03-11 22:08:39 +0800

    feat(service/mod): add event

--- examples/server.rs
+++ examples/server.rs
@@ -4,13 +4,13 @@ use futures::prelude::*;
 use tokio::net::TcpListener;
 use tracing::info;
 
-use kv::{memory::MemTable, CommandRequest, CommandResponse, Service};
+use kv::{memory::MemTable, CommandRequest, CommandResponse, Service, ServiceInner};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let service: Service = Service::new(MemTable::new());
+    let service: Service = ServiceInner::new(MemTable::default()).into();
     let addr = "127.0.0.1:9527";
     let listener = TcpListener::bind(addr).await?;
     info!("Start listening on {}", addr);
